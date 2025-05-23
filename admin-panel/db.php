1d95c90bb839e4609982abb0416939a48f932e2b
unce2025 <cargoexpress.un@gmail.com>
2025-05-23 20:21:50 +0800

    Update db.php

--- admin-panel/db.php
+++ admin-panel/db.php
@@ -1,1 +1,12 @@
+<?php
+$host = "sql3.freesqldatabase.com";
+$dbname = "sql3780760";
+$username = "your_username";
+$password = "your_password";
 
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
