9e458dd11c8e9d3825437174e04b3d2ca7201850
unce2025 <cargoexpress.un@gmail.com>
2025-05-23 20:22:26 +0800

    Update admin.php

--- admin-panel/admin.php
+++ admin-panel/admin.php
@@ -1,1 +1,23 @@
-
+<?php include 'db.php'; ?>
+<h2>All Shipments</h2>
+<a href="add.php">Add New Shipment</a>
+<table border="1">
+<tr>
+    <th>Tracking #</th><th>Status</th><th>Shipper</th><th>Receiver</th><th>Action</th>
+</tr>
+<?php
+$result = $conn->query("SELECT * FROM shipments");
+while ($row = $result->fetch_assoc()) {
+    echo "<tr>
+        <td>{$row['tracking_number']}</td>
+        <td>{$row['status']}</td>
+        <td>{$row['shipper_name']}</td>
+        <td>{$row['receiver_name']}</td>
+        <td>
+            <a href='edit.php?id={$row['tracking_number']}'>Edit</a> | 
+            <a href='delete.php?id={$row['tracking_number']}'>Delete</a>
+        </td>
+    </tr>";
+}
+?>
+</table>
